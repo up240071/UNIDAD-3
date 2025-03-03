682189543590f7a3c693ed790727daada892358d
up240071 <up240071@alumnos.upa.edu.mx>
2025-03-03 14:58:50 -0600

    Add files via upload

---
+++ 03_dayeje.py
@@ -0,0 +1,24 @@
+#Boolean
+print(True)
+print(False)
+
+#Exercises1 Day3
+
+#Number1
+#Declare your age as integer variable
+print ('Age: 24')
+
+#Number2
+#Declare you heigth as float variable
+print ('Heigth: 1.52')
+
+#Number3
+#Declare a variable that store a complex number
+print('Complex number:',1 + 1j) 
+
+#Number4
+#Write a script that prompts the user to enter base he
+base=int (input('Enter base:  '))
+Heigth=int (input('Enter heigth  '))
+area= int (0.5*base*Heigth)
+print ('THE AREA OF THE TRIANGLE IS:  ', area)
